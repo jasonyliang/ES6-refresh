166ff6748e090858424ee0cddaf343bad66192fc
Jason Liang <jjjasonliang@gmail.com>
2019-01-14 22:13:23 +0530

    let and const

---
+++ let_const.jsx
@@ -0,0 +1,20 @@
+// let for variable values (var still works tho)
+// const for constant values
+var myName = "J";
+console.log(myName);
+
+myName = "Jason";
+console.log(myName);
+// same thing with let
+let myName1 = "J";
+console.log(myName1);
+
+myName1 = "Jason";
+console.log(myName1);
+
+// will return an error with const
+const myName2 = "J";
+console.log(myName2);
+
+// myName2 = "Jason";
+// console.log(myName2);
