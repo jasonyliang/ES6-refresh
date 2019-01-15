81740a4c23eb4fc49fd211c1f3436dbc1a123048
Jason Liang <jjjasonliang@gmail.com>
2019-01-15 07:59:38 +0530

    destructuring

---
+++ destructuring.jsx
@@ -0,0 +1,7 @@
+const numbers = [1, 2, 3, 4, 5];
+[num1, num2, , num4] = numbers;
+console.log(num1, num2, num4);
+
+const person = {name: 'Jason', gender: 'Male'}
+{name} = person
+console.log(name)
--- spread_rest.jsx
+++ spread_rest.jsx
@@ -17,7 +17,9 @@ console.log(newPerson);
 
 // functions
 const filter = (...args) => {
-  return args.filter(el => el === 1);
+  return args.filter(el => {
+    return el === 1;
+  });
 };
 
 console.log(filter(1, 2, 3, 4, 5, 1));
